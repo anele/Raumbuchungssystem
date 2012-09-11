7cab8f36dcf3db36cd38f3f768a0aabac950086e
aykurun <aykurun@htwg-konstanz.de>
2012-09-11 11:33:13 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raumbuchungssystem::Application.config.secret_token = 'f0fd83daa313b9986f2fdb95bc3eb58dc6b2f41c310e5a268cf3bb788a2f34f347a43a3f8bf6d2e1aebdaf090d85022da8096be61dc0a7c9d50d099220d0ecd1'
