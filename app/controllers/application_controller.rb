4a572d38f9b053e89a3e48acdf97cf00557312e2
anele <helorenz@htwg-konstanz.de>
2013-02-18 19:18:25 +0100

    Revert "HTML-Seiten sehr geändert und fürs Stylen für Mobile-Endgeräte angepasst. also, CSS für Handy ist noch nicht fertig.  Bei 3 HTML-Seite bin mir nicht sicher, ob die benutzt werden oder nicht(Die Überschriften sind endsprechend markiert)."
    This reverts commit 26afb73875d3c48af23e8d8cc1ef58b3eca2aba7.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :prepare_for_mobile
-  
   private 
   
   def current_user
@@ -9,19 +7,4 @@ class ApplicationController < ActionController::Base
   end
   
   helper_method :current_user
-  
-  def mobile_device?
-    if session[:mobile_param]
-      session[:mobile_param] == "1"
-    else
-      request.user_agent =~ /Mobile|webOS/
-    end
-  end
-  helper_method :mobile_device?
-  
-  def prepare_for_mobile
-    session[:mobile_param] = params[:mobile] if params[:mobile]
-    request.format = :mobile if mobile_device?
-  end
-
 end
